124e4ff3f74a6e534bc0d60e0cb3fcd384f71c71
chrisbrown1982 <chrisbrown.guitar@googlemail.com>
2025-04-02 15:15:22 +0100

    sumEuler DC

--- Idris/sumEuler.erl
+++ Idris/sumEuler.erl
@@ -16,6 +16,23 @@ euler(N) -> length( lists:filter(fun(X) -> ?MODULE:relPrime(N,X) end, mkList(N))
 
 sumEuler(N) -> io:format("~p~n", [lists:sum(lists:map(fun ?MODULE:euler/1,mkList(N)))]).
 
+sumEulerDCP({T, []}) -> 0;
+sumEulerDCP({T, [X]}) -> euler(X);
+sumEulerDCP({T, Xs}) when length(Xs) < T -> sumEulerDC(Xs); 
+sumEulerDCP({T, Xs}) -> 
+	{Left, Right} = lists:split(length(Xs) div 2, Xs),
+        S1 = play2:app_stream(play2:process(fun sumEulerDCP/1), {T, Left}),
+	S2 = play2:app_stream(play2:process(fun sumEulerDCP/1), {T, Right}),
+	play2:sync_stream(S1) + play2:sync_stream(S2).
+
+sumEulerDC([]) -> 0;
+sumEulerDC([X]) -> euler(X);
+sumEulerDC(Xs) ->
+        {Left, Right} = lists:split(length(Xs) div 2, Xs),
+        S1 = sumEulerDC(Left),
+        S2 = sumEulerDC(Right),
+        S1 + S2.
+
 run_seq(X) ->
 	  io:format("SumEuler Seq: ~p~n", [sk_profile:benchmark(fun ?MODULE:sumEuler/1, [X], 1)]),
 	  io:format("Done with seq. ~n").
